65d78b892d50e1c6edfb1f112937e4c358199fca
feuster <alexander.feuster@gmail.com>
2013-12-18 15:35:14 +0100

    CdxUtils updated but still WiP

--- CdxUtils.pas
+++ CdxUtils.pas
@@ -4,7 +4,7 @@ unit CdxUtils;
 Unit Information:
 ------------------------------------------------------------------------
 Name:       CdxUtils
-Version:    1.0
+Version:    1.1
 Purpose:    Set of additional helper functions
 Copyright:  Alexander Feuster
 Contact:    alexander.feuster@gmail.com
@@ -22,20 +22,31 @@ Version History:
                     function Split()
                     function SubnetFromIP()
 1.0b  17.12.2013    added Github URL
-}
+1.1   18.12.2013    function HexToBinStr()
+                    function IntToBinStr()
+                    procedure WindowsLogoff()
+                    procedure WindowsRestart()
+                    procedure WindowsShutdown()
 
+}
 {$mode objfpc}{$H+}
 
 interface
 
 uses
-  Classes, SysUtils, StrUtils, Windows;
+  Classes, SysUtils, StrUtils, Windows, Forms, Process;
 
 function GetFileVersion(Filename: String; const ShortForm: Boolean = false): String;
+function HexToBinStr(HexString: String): String;
+function IntToBinStr(Value: Integer): String;
 function UnicodeStringReplace(const S, OldPattern, NewPattern: UnicodeString;  Flags: TReplaceFlags): UnicodeString;
 function UTF8Chr(Unicode: Cardinal): UTF8String;
 function Split(Delimiter: Char; Text: String): TStrings;
 function SubnetFromIPv4(IP: String): String;
+procedure WindowsLogoff(const forced: Boolean = true; const Delay: Byte = 1);
+procedure WindowsRestart(const forced: Boolean = true; const Delay: Byte = 1);
+procedure WindowsShutdown(const forced: Boolean = true; const Delay: Byte = 1);
+
 
 implementation
 
@@ -80,6 +91,42 @@ begin
         FreeMemory(pFileInfo);
       end;
     end;
+end;
+
+function HexToBinStr(HexString: String): String;
+//Converts a hexadecimal String to a binary String
+const
+  //String array for the value Bits from 0 to 15
+  HexBits: array [0..15] of String =
+    ('0000', '0001', '0010', '0011',
+     '0100', '0101', '0110', '0111',
+     '1000', '1001', '1010', '1011',
+     '1100', '1101', '1110', '1111');
+
+var
+  Index: Integer;
+
+begin
+  //remove trailing $ in case of
+  if LeftStr(HexString,1)='$' then
+    HexString:=RightStr(HexString, Length(HexString)-1);
+  //remove trailing # in case of
+  if LeftStr(HexString,1)='#' then
+    HexString:=RightStr(HexString, Length(HexString)-1);
+  //remove trailing 0x in case of
+  if LeftStr(HexString,2)='0x' then
+    HexString:=RightStr(HexString, Length(HexString)-2);
+
+  //add for every Hexadecimal char the according binary string part
+  Result:='';
+  for Index:=Length(HexString) DownTo 1 do //start String creation with MSB (Most Significat Bit = most left Bit)
+    Result:=HexBits[StrToInt('$'+HexString[Index])]+Result;
+end;
+
+function IntToBinStr(Value: Integer): String;
+//Converts a hexadecimal String to a binary String
+begin
+  result:=HexToBinStr(IntToHex(Value,SizeOf(Integer)));
 end;
 
 function UnicodeStringReplace(const S, OldPattern, NewPattern: UnicodeString;  Flags: TReplaceFlags): UnicodeString;
@@ -154,6 +201,62 @@ begin
     Result:=''
   else
     Result:=AnsiLeftStr(IP,LastDelimiter('.',IP));
+end;
+
+procedure Call_Shutdown_Exe(Mode: Byte; const forced: Boolean = true; const Delay: Byte = 1);
+//Function calls of Windows commandline tool "shutdown.exe"
+var
+  Process: TProcess;
+  Para: String;
+
+begin
+  //Create a process for Windows "shutdown.exe"
+  Process:=TProcess.Create(nil);
+  Process.Executable:='shutdown.exe';
+  Para:=' ';
+  if Mode=1 then
+    Para:=Para+'-l'                      //logoff with a timed delay of 1 sec
+  else if Mode=2 then
+    Para:=Para+'-r'                      //restart with a timed delay of 1 sec
+  else
+    Para:=Para+'-s';                     //shutdown with a timed delay of 1 sec
+  if forced=true then
+    Para:=Para+' -f';                     //forced
+  if Delay>99 then
+    Para:=Para+' -t 99'                   //add delay restricted to maximum of 99
+  else if Delay<10 then
+    Para:=Para+' -t 0'+IntToStr(Delay)    //add delay below 10 seconds with trailing 0
+  else
+    Para:=Para+' -t '+IntToStr(Delay);    //add delay
+  Process.Parameters.Text:=Para;
+  Process.Options:=Process.Options+[];
+  Process.ShowWindow:=swoShow;
+  Process.Priority:=ppNormal;
+
+  //run and free process
+  Process.Execute;
+  Process.Free;
+
+  //close application to prevent Windows not shutting down due to a still running task
+  Application.Terminate;
+end;
+
+procedure WindowsLogoff(const forced: Boolean = true; const Delay: Byte = 1);
+//Restart Windows
+begin
+  Call_Shutdown_Exe(1, forced, Delay);
+end;
+
+procedure WindowsRestart(const forced: Boolean = true; const Delay: Byte = 1);
+//Restart Windows
+begin
+  Call_Shutdown_Exe(2, forced, Delay);
+end;
+
+procedure WindowsShutdown(const forced: Boolean = true; const Delay: Byte = 1);
+//Shutdown Windows
+begin
+  Call_Shutdown_Exe(0, forced, Delay);
 end;
 
 end.
